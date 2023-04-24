b1e7ed7cfdbd687bc9b1d3d6ffefb049c24716a0
yun <austru00@gmail.com>
2023-04-24 16:26:00 +0900

    2.2 Inheritance

--- day1/2OOP WITH PYTHON.py
+++ day1/2OOP WITH PYTHON.py
@@ -1,9 +1,20 @@
-class Player:
-    def __init__(self, name, xp):
+class Human:
+    def __init__(self, name):
         self.name=name
-        self.xp=xp
+        
     def sayHello(self):
         print(f"Hello my name is {self.name}")
+
+class Player(Human):
+    def __init__(self, name, xp):
+        self.xp=xp
+
+class Fan(Human):
+    def __init__(self, name, fav_team):
+        self.fav_team=fav_team
         
-mg=Player('mg',1000)
-mg.sayHello()
+        
+mg_player=Player("mg",10)
+mg_player.sayHello()
+mg_fan=Fan("mg_fan","dont know")
+mg_fan.sayHello()
