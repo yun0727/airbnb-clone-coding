106ad125bf2f82978f5bb93ebb4193fff7901780
yun <austru00@gmail.com>
2023-05-04 16:58:38 +0900

    10.2 api_view

--- aribnb-clone-backend/categories/views.py
+++ aribnb-clone-backend/categories/views.py
@@ -1,7 +1,12 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
 
 
+@api_view()
 def categories(request):
-    all_categories = Category.objects.all()
-    return JsonResponse({"ok": True})
+    return Response(
+        {
+            "ok": True,
+        }
+    )
