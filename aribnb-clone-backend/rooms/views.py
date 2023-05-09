d32e26effab287daf4a62f2a39172afec028effd
yun <austru00@gmail.com>
2023-05-09 16:49:51 +0900

    11.15 File Uploads

--- aribnb-clone-backend/rooms/views.py
+++ aribnb-clone-backend/rooms/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework.views import APIView
 from django.db import transaction
 from rest_framework.status import HTTP_204_NO_CONTENT
@@ -192,7 +193,7 @@ class RoomReviews(APIView):
             page = int(page)
         except ValueError:
             page = 1
-        page_size = 3
+            page_size = settings.PAGE_SIZE
         start = (page - 1) * page_size
         end = start + page_size
 
@@ -202,3 +203,8 @@ class RoomReviews(APIView):
             many=True,
         )
         return Response(serializer.data)
+
+
+class RoomPhotos(APIView):
+    def post(self, request, pk):
+        pass
