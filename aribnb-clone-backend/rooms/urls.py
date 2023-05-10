9f115a8cc63f8c52fe2e0924d7fcd990efc44302
yun <austru00@gmail.com>
2023-05-10 11:33:22 +0900

    11.22 Bookings

--- aribnb-clone-backend/rooms/urls.py
+++ aribnb-clone-backend/rooms/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path("", views.Rooms.as_view()),
     path("<int:pk>", views.RoomDetail.as_view()),
     path("<int:pk>/reviews", views.RoomReviews.as_view()),
-    # sdkfjas
     path("<int:pk>/photos", views.RoomPhotos.as_view()),
+    path("<int:pk>/bookings", views.RoomBookings.as_view()),
     path("amenities/", views.Amenities.as_view()),
     path("amenities/<int:pk>", views.AmenitiesDetail.as_view()),
 ]
