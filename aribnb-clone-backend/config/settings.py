347f48b98f580b257e11597c6bc924b9f3c17a25
yun <austru00@gmail.com>
2023-05-12 02:14:05 +0900

    15.1 Custom Authentication

--- aribnb-clone-backend/config/settings.py
+++ aribnb-clone-backend/config/settings.py
@@ -151,3 +151,10 @@ MEDIA_ROOT = "uploads"
 MEDIA_URL = "user-uploads/"
 
 PAGE_SIZE = 3
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "config.authentication.TrustMeBroAuthentication",
+    ]
+}
