f21c1443cd9a14a759a976e2f608701ccbf195c3
yun <austru00@gmail.com>
2023-05-13 00:13:30 +0900

    15.6 Environment Files

--- aribnb-clone-backend/config/settings.py
+++ aribnb-clone-backend/config/settings.py
@@ -11,16 +11,22 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-idm^6-tu%_xt04t#h5&3=*i@eji+-y2wy^7+)7srd8b^b$qi16"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
