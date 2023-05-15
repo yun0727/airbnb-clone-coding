8a78563ff2eee6195716ec5ede40b177b667b4be
yun <austru00@gmail.com>
2023-05-15 15:26:05 +0900

    17.4 Not Found Page

--- airbnb-clone-frontend/src/router.tsx
+++ airbnb-clone-frontend/src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
 import Root from "./components/Root"
 import Home from "./routes/Home";
-import Users from "./routes/Users";
+import NotFound from "./routes/NotFound";
 
 
 const router=
@@ -9,14 +9,11 @@ createBrowserRouter([
   {
   path:"/",
   element: <Root/>,
+  errorElement: <NotFound/>,
   children: [
     {
       path: "",
       element: <Home />,
-    },
-    {
-      path: "users",
-      element: <Users />,
     },
   ],
   }
